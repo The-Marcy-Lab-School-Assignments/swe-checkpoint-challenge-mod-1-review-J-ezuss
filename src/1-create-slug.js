56ea9a5930931c0c96dfd3fbc53838bd83ae309f
[Jesus Martinez] <[jesand13@gmail.com]>
2025-12-11 12:47:51 -0500

    short response

--- src/1-create-slug.js
+++ src/1-create-slug.js
@@ -1,8 +1,6 @@
 // Return a URL-friendly "slug": lowercase with hyphens instead of spaces.
 // Return null if the title contains banned characters: "!", "#", "?"
-const createSlug = (title) => {
-
-};
+const createSlug = (title) => {};
 
 module.exports = {
   createSlug,
